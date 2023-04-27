2dd5db88605a4f0c3b16a1e9c093426c7a8dfe67
Anatoly Kostin <anatoly@kostin.co.uk>
2023-04-27 16:22:04 +0100

    Added Price Utils

---
+++ Utils/PriceUtils.cs
@@ -0,0 +1,41 @@
+﻿using DataServices.Models;
+using DataServices.Models.Clients;
+using ReportGrpc;
+
+namespace DataServices.Utils;
+public static class PriceUtils
+    {
+        public static int CalcSpread(BidAskModel bidAsk, int instrumentDigits)
+        {
+            var multiplier = Math.Pow(10, instrumentDigits);
+            return (int)(bidAsk.Ask * multiplier - bidAsk.Bid * multiplier);
+        }
+
+        public static int CalcSpread(UnfilteredBidAskModel bidAsk, int instrumentDigits)
+        {
+            var multiplier = Math.Pow(10, instrumentDigits);
+            return (int)(bidAsk.Ask * multiplier - bidAsk.Bid * multiplier);
+        }
+
+        public static int CalcTicks(double priceBefore, double priceAfter, int instrumentDigits)
+        {
+            var multiplier = Math.Pow(10, instrumentDigits);
+            return (int)(priceAfter * multiplier - priceBefore * multiplier);
+        }
+        
+        public static double GetOrderPrice(ReportsFlowsPositionSide op, BidAskModel bidAsk)
+        {
+            return op == ReportsFlowsPositionSide.Buy
+                ? bidAsk.Bid
+                : bidAsk.Ask;
+        }
+
+        public static double CalcProfitLossByPrice(InvestmentPositionModel order, double nowPrice)
+        {
+            var opSide = order.Side == ReportsFlowsPositionSide.Buy ? 1 : -1;
+
+            order.Profit = (nowPrice / order.OpenPrice - 1) * order.InvestAmount * order.Leverage * opSide;
+            return order.Profit;
+        }
+    }
+
