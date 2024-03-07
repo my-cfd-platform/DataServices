b70dec6a0e9fa1bcd5ff787a79f476b0b710b69b
Anatoly Kostin <anatoly@kostin.co.uk>
2024-03-07 12:35:14 +0000

    Fixed json model

--- MyNoSql/Models/ProductSettings/ProductBrandSettingsEntity.cs
+++ MyNoSql/Models/ProductSettings/ProductBrandSettingsEntity.cs
@@ -1,27 +1,43 @@
 ﻿using DataServices.MyNoSql.Interfaces.ProductSettings;
 using MyNoSqlServer.Abstractions;
+using Newtonsoft.Json;
 
 namespace DataServices.MyNoSql.Models.ProductSettings;
 
 public class ProductBrandSettingsEntity : MyNoSqlDbEntity, IProductBrandSettings
 {
     public string Id => RowKey = "brand";
-
+    [JsonProperty("logoUrl")]
     public string LogoUrl { get; set; } = string.Empty;
+    [JsonProperty("policyUrl")]
     public string PolicyUrl { get; set; } = string.Empty;
+    [JsonProperty("termsUrl")]
     public string TermsUrl { get; set; } = string.Empty;
+    [JsonProperty("withdrawFaqUrl")]
     public string WithdrawFaqUrl { get; set; } = string.Empty;
+    [JsonProperty("aboutUrl")]
     public string AboutUrl { get; set; } = string.Empty;
+    [JsonProperty("supportUrl")]
     public string SupportUrl { get; set; } = string.Empty;
+    [JsonProperty("brandName")]
     public string BrandName { get; set; } = string.Empty;
+    [JsonProperty("brandCopyrights")]
     public string BrandCopyrights { get; set; } = string.Empty;
+    [JsonProperty("gaAsAccount")]
     public string GaAsAccount { get; set; } = string.Empty;
+    [JsonProperty("mixPanelToken")]
     public string MixPanelToken { get; set; } = string.Empty;
+    [JsonProperty("faviconUrl")]
     public string FaviconUrl { get; set; } = string.Empty;
+    [JsonProperty("androidAppId")]
     public string AndroidAppId { get; set; } = string.Empty;
+    [JsonProperty("androidAppLink")]
     public string AndroidAppLink { get; set; } = string.Empty;
+    [JsonProperty("iosAppId")]
     public string IosAppId { get; set; } = string.Empty;
+    [JsonProperty("iosAppLink")]
     public string IosAppLink { get; set; } = string.Empty;
+    [JsonProperty("mobileAppLogo")]
     public string MobileAppLogo { get; set; } = string.Empty;
 
     public static string GeneratePartitionKey()
