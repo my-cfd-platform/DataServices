415611cc24c00f183fb3b86029af7eca25c60647
Anatoly Kostin <anatoly@kostin.co.uk>
2024-03-11 16:39:03 +0000

    Batch extention added

---
+++ Extensions/EnumerableExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace DataServices.Extensions;
+
+public static class EnumerableExtension
+{
+    public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> items,
+        int maxItems)
+    {
+        return items.Select((item, inx) => new { item, inx })
+            .GroupBy(x => x.inx / maxItems)
+            .Select(g => g.Select(x => x.item));
+    }
+}
